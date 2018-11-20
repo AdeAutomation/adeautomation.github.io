7e62d32131bac5c33db1c82987e5525726470ba4
Ade <ade.adebanjo@greenfinch.ie>
2018-11-20 17:38:12 +0000

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 /////////////////////////////////////////////////////////////////////////////
 //
 
+
+
+function validation(){
+var firstname = document.getElementById("firstname").value;
+if (firstname == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+
 function submitForm(){
 	document.getElementById('cform').submit();
 	alert("Thank you. Your form has been successfully submitted");
 
-	
-	
 }
 
