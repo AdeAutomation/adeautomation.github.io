a2fb0edaf8cbd46c5059005bdbfc8edaef52b07a
Ade <ade.adebanjo@greenfinch.ie>
2018-11-21 16:54:39 +0000

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
-/////////////////////////////////////////////////////////////////////////////
-//
 
 
 
-function validation(){
+function validateForm(){
 var firstname = document.getElementById("firstname").value;
-if (firstname == "") {
-        alert("Name must be filled out");
+var lastname = document.getElementById("lastname").value;
+var email = document.getElementById("email").value;
+var tel = document.getElementById("tel").value;
+var telInput = tel.replace(/[^\d]/g, '');
+
+if (firstname == "" || lastname == "" || email == "") {
+        alert("Please ensure you have entered your names and a valid email address");
         return false;
     }
+	
+	
 }
 
 
-function submitForm(){
-	document.getElementById('cform').submit();
-	alert("Thank you. Your form has been successfully submitted");
+
+
+ function promptMessage(){
+	 var person = prompt("Please confirm your name before resetting form");
+	 if(person != null){
+		 document.getElementById("submit").innerHTML = person;
+		 
+	 }
+	 
 
 }
 
