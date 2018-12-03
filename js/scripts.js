5cae20139a2a144f2f8af7e9aead9011f95b8425
AdeAutomation <yemi4ade@yahoo.com>
2018-12-03 22:48:46 +0000

    Update js and css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
-
+/**
+This function validates contact form inputs
+**/
 
 function validateForm(){
 var firstname = document.getElementById("firstname").value;
@@ -9,18 +11,183 @@ var tel = document.getElementById("tel").value;
 var telInput = tel.replace(/[^\d]/g, '');
 
 if (firstname == "" || lastname == "" || email == "") {
-        alert("Please ensure you have entered your names and a valid email address");
-        return false;
-    }
-	
-	
+		alert("Please ensure you have entered your names and a valid email address");
+		return false;
+	}
 }
 
 
 
- function promptMessage(){
-	 var person = prompt("Please confirm your name before resetting form");
-	 if(person != null){
-		 document.getElementById("submit").innerHTML = person;
-	 
- }
+
+
+
+/**
+This function uses if else method to peform arithmetic operations and validate input
+**/
+function calculateifelse(){
+
+alert("This is a calculator. Click OK to continue");
+
+var a = prompt("Enter the first number: ");
+if(a == "" || isNaN(a))
+{
+alert("Please enter a valid number and try again")
+return false;
+}
+
+
+var b = prompt("Enter the second number: ");	
+if(b == "" || isNaN(b))
+{
+alert("Please enter a valid number and try again")
+return false;		
+}
+
+var operator = prompt("Enter Operator Symbol: ");
+
+
+if(operator == "+")
+{
+var result = parseInt(a) + parseInt(b);
+}
+else
+{
+if(operator == "-")
+{
+	var result = parseInt(a) - parseInt(b);	
+}
+else
+{
+	if(operator == "/")
+	{
+		var result = parseInt(a) / parseInt(b);	
+	}
+	else
+	{
+		if(operator == "*")
+		{
+			var result = parseInt(a) * parseInt(b);	
+		}
+		else
+		{
+			alert("Operator Not Allowed");
+		}
+	}
+}
+}
+
+alert("Answer is: " + result);
+
+}
+
+
+
+
+/**
+This function uses if else-if method to peform arithmetic operations and validate input
+**/
+function calculateIfElseIF(){
+
+alert("This is a calculator. Click OK to continue");
+
+var a = prompt("Enter the first number: ");
+if(a == "" || isNaN(a))
+{
+alert("Please enter a valid number and try again")
+return false;
+}
+
+
+var b = prompt("Enter the second number: ");	
+if(b == "" || isNaN(b))
+{
+alert("Please enter a valid number and try again")
+return false;		
+}
+
+var operator = prompt("Enter Operator Symbol: ");
+
+if(operator == "+")
+{
+	var result = parseInt(a) + parseInt(b);
+}
+
+else if (operator == "-")
+{
+	var result = parseInt(a) - parseInt(b);
+}
+
+else if (operator == "/")
+{
+	var result = parseInt(a) / parseInt(b);
+}
+
+else if(operator == "*")
+{
+	var result = parseInt(a) * parseInt(b);
+}
+
+else
+{
+	var result= "Operator Not Permitted";
+}
+
+
+alert("THE RESULT is : " + result);
+}
+
+
+
+
+
+
+
+/**
+This function uses switch-case method to peform arithmetic operations and validate input
+**/
+function calculateSwitchCase(){
+
+alert("This is a calculator. Click OK to continue");
+
+var a = prompt("Enter the first number: ");
+if(a == "" || isNaN(a))
+{
+alert("Please enter a valid number and try again")
+return false;
+}
+
+
+var b = prompt("Enter the second number: ");	
+if(b == "" || isNaN(b))
+{
+alert("Please enter a valid number and try again")
+return false;		
+}
+
+
+var operator = prompt("Enter Operator Symbol: ");
+
+switch(operator)
+{
+case "+":
+	var result = parseInt(a) + parseInt(b);
+	break;
+	
+case "-":
+	var result = parseInt(a) - parseInt(b);
+	break;
+	
+case "/":
+	var result = parseInt(a) / parseInt(b);
+	break;
+case "*":
+	var result = parseInt(a) * parseInt(b);
+	break;
+default:
+	var result = "Operator Not Permitted";
+}
+
+alert("MY ANSWER IS : " + result);
+}
+
+
