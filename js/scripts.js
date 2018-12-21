46c23d8e7f003fcaba1f41b64418f1e72fd09369
Ade <ade.adebanjo@greenfinch.ie>
2018-12-21 17:36:12 +0000

    Update calculator.htlm file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
 
 
+
+/**
+INDEX PAGE - This function is used in the loop game to display multiplation table for input numbers
+**/
+
+
+ function loopNumbers()
+ {
+  var x = parseInt(document.getElementById("x").value);
+  var y = parseInt(document.getElementById("y").value);
+  
+  var totalOutput = "";
+  
+  var j = 1;
+  for(i = 1; i<= y; i = x*j)
+  {
+   var c = i;
+   totalOutput = totalOutput + x + " * "+ j +" = "+ i +"<br>";
+   j = j + 1;
+  }
+  document.getElementById("demo").innerHTML = totalOutput;
+ }
+
+
+
+
 /**
 This function gets that user enters username and password to login
 **/
-function validateLoginForm(){
+function validateLoginForm()
+{
 	
     var email = document.getElementById("loginemail").value;
 	var password = document.getElementById("password").value;
@@ -25,8 +52,8 @@ if (email == "" || password == "")
 /**
 This function gets the username of the user and displays it on the 'My Account' page 
 **/
-function loggedInUser(email){
-//var username = document.getElementById("loginemail").value;
+function loggedInUser(email)
+{
 document.getElementById("loggedinuser").innerHTML = "Welcome, " + localStorage.getItem("storageName");
 
 }
@@ -36,7 +63,8 @@ document.getElementById("loggedinuser").innerHTML = "Welcome, " + localStorage.g
 This function validates contact form inputs
 **/
 
-function validateForm(){
+function validateForm()
+{
 var firstname = document.getElementById("firstname").value;
 var lastname = document.getElementById("lastname").value;
 var email = document.getElementById("email").value;
@@ -66,123 +94,97 @@ return false;
 
 
 
-
-
-
 /**
-This function uses if else method to peform arithmetic operations and validate input
+CALCULATOR PAGE - This function loads the calculator functionality
 **/
-function calculateifelse(){
-
-alert("This is a calculator. Click OK to continue");
-
-var a = prompt("Enter the first number: ");
-if(a == "" || isNaN(a))
+function calculator()
 {
-alert("Please enter a valid number and try again")
-return false;
-}
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
 
-
-var b = prompt("Enter the second number: ");	
-if(b == "" || isNaN(b))
-{
-alert("Please enter a valid number and try again")
-return false;		
-}
-
-var operator = prompt("Enter Operator Symbol: ");
-
-
-if(operator == "+")
-{
-var result = parseInt(a) + parseInt(b);
-}
-else
-{
-if(operator == "-")
-{
-	var result = parseInt(a) - parseInt(b);	
-}
-else
-{
-	if(operator == "/")
-	{
-		var result = parseInt(a) / parseInt(b);	
-	}
-	else
-	{
-		if(operator == "*")
-		{
-			var result = parseInt(a) * parseInt(b);	
+// Add onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+		// Get the input and button values
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		
+		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+		// If clear key is pressed, erase everything
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
 		}
-		else
-		{
-			alert("Operator Not Allowed");
+		
+		// If eval key is pressed, calculate and display the result
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			
+			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.innerHTML = eval(equation);
+				
+			decimalAdded = false;
 		}
-	}
-}
-}
-
-alert("Answer is: " + result);
-
-}
-
-
-
-
-/**
-This function uses if else-if method to peform arithmetic operations and validate input
-**/
-function calculateIfElseIF(){
-
-alert("This is a calculator. Click OK to continue");
-
-var a = prompt("Enter the first number: ");
-if(a == "" || isNaN(a))
-{
-alert("Please enter a valid number and try again")
-return false;
-}
-
-
-var b = prompt("Enter the second number: ");	
-if(b == "" || isNaN(b))
-{
-alert("Please enter a valid number and try again")
-return false;		
-}
-
-var operator = prompt("Enter Operator Symbol: ");
-
-if(operator == "+")
-{
-	var result = parseInt(a) + parseInt(b);
-}
-
-else if (operator == "-")
-{
-	var result = parseInt(a) - parseInt(b);
-}
-
-else if (operator == "/")
-{
-	var result = parseInt(a) / parseInt(b);
-}
-
-else if(operator == "*")
-{
-	var result = parseInt(a) * parseInt(b);
-}
-
-else
-{
-	var result= "Operator Not Permitted";
+		
+		// Basic functionality of the calculator is complete. But there are some problems like 
+		// 1. No two operators should be added consecutively.
+		// 2. The equation shouldn't start from an operator except minus
+		// 3. not more than 1 decimal should be there in a number
+		
+		// We'll fix these issues using some simple checks
+		
+		// indexOf works only in IE9+
+		else if(operators.indexOf(btnVal) > -1) {
+			// Operator is clicked
+			// Get the last character from the equation
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			// Only add operator if input is not empty and there is no operator at the last
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			// Allow minus if the string is empty
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			// Replace the last operator (if exists) with the newly pressed operator
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+		
+		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		// if any other key is pressed, just append it
+		else {
+			input.innerHTML += btnVal;
+		}
+		
+		// prevent page jumps
+		e.preventDefault();
+	} 
 }
 
 
-alert("THE RESULT is : " + result);
-}
 
 
 
@@ -193,11 +195,10 @@ alert("THE RESULT is : " + result);
 /**
 This function uses switch-case method to peform arithmetic operations and validate input
 **/
-function calculateSwitchCase(){
+function calculate(){
 
-alert("This is a calculator. Click OK to continue");
 
-var a = prompt("Enter the first number: ");
+var a = document.getElementById("num1").value;
 if(a == "" || isNaN(a))
 {
 alert("Please enter a valid number and try again")
@@ -205,7 +206,7 @@ return false;
 }
 
 
-var b = prompt("Enter the second number: ");	
+var b = document.getElementById("num2").value;	
 if(b == "" || isNaN(b))
 {
 alert("Please enter a valid number and try again")
@@ -213,7 +214,7 @@ return false;
 }
 
 
-var operator = prompt("Enter Operator Symbol: ");
+var operator = document.getElementById("operator").value;	
 
 switch(operator)
 {
@@ -235,7 +236,31 @@ default:
 	var result = "Operator Not Permitted";
 }
 
-alert("MY ANSWER IS : " + result);
+document.getElementById("result").value = result;
 }
 
 
+
+
+
+
+
+
+/**
+This jquery function adds a slide show effect to the banners
+**/
+$("#slideshow > div:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+
+
+
+
+
+}
