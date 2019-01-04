12d2d794fe0d922bf6b21787de60ab6fc302ecc2
AdeAutomation <yemi4ade@yahoo.com>
2019-01-04 00:53:05 +0000

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -129,20 +129,7 @@ else
 }
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
+ 
 
 /**
 This function gets the username of the user and displays it on the 'My Account' page 
@@ -207,8 +194,7 @@ for(var i = 0; i < keys.length; i++) {
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		
-		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
-		// If clear key is pressed, erase everything
+		// Append the key values (btnValue) to the input string and  get the result. If clear key is pressed, erase everything
 		if(btnVal == 'C') {
 			input.innerHTML = '';
 			decimalAdded = false;
@@ -219,7 +205,7 @@ for(var i = 0; i < keys.length; i++) {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 			
-			// Replace all instances of x and รท with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			// Replace all instances of x and รท  with * and / respectively.
 			equation = equation.replace(/x/g, '*').replace(/รท/g, '/');
 			
 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
@@ -232,12 +218,6 @@ for(var i = 0; i < keys.length; i++) {
 			decimalAdded = false;
 		}
 		
-		// Basic functionality of the calculator is complete. But there are some problems like 
-		// 1. No two operators should be added consecutively.
-		// 2. The equation shouldn't start from an operator except minus
-		// 3. not more than 1 decimal should be there in a number
-		
-		// We'll fix these issues using some simple checks
 		
 		// indexOf works only in IE9+
 		else if(operators.indexOf(btnVal) > -1) {
@@ -255,14 +235,14 @@ for(var i = 0; i < keys.length; i++) {
 			
 			// Replace the last operator (if exists) with the newly pressed operator
 			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
+				
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
 			
 			decimalAdded =false;
 		}
 		
-		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
+		// Add decimal
 		else if(btnVal == '.') {
 			if(!decimalAdded) {
 				input.innerHTML += btnVal;
@@ -270,7 +250,6 @@ for(var i = 0; i < keys.length; i++) {
 			}
 		}
 		
-		// if any other key is pressed, just append it
 		else {
 			input.innerHTML += btnVal;
 		}
